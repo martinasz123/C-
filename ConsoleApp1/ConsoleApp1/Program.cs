ebd10b4da707ac42ba1cca0d8c0b0db79eb47d48
MartinSomogyi <martinasz123@gmail.com>
2025-01-19 14:33:37 +0000

    Setting up environment

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Practice
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
